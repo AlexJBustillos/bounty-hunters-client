a42a0d800e7615de910b5f8b0ac26d2030c5cf86
alexjbustillos <alexjbustillos@gmail.com>
2020-12-16 15:50:10 -0500

    formField added

---
+++ src/components/FormField.js
@@ -0,0 +1,13 @@
+function FormField(props) {
+    return (
+        <div>
+            <label htmlFor={props.labelLink}>{props.displayLabel}</label>
+            <input 
+                name={props.labelLink} value={props.value} 
+                onChange={props.handler}
+            />
+        </div>
+    )
+}
+
+export default FormField
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,13 @@ import BountyLink from '../components/BountyLink'
 
 class Home extends Component {
     render() {
+        const bountyLinks = this.props.bounties.map((b) => {
+            return <BountyLink name={b.name} bountyId={b._id} key={b._id}/>
+        })
         return (
             <div>
                 <h2>Home page!</h2>
-                <BountyLink name={'Yosemite Sam'} bountyId={'asdf1234'}/>
+                {bountyLinks}
             </div>
         )
     }
