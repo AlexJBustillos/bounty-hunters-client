6af9e10aaf3b5e8fc4f7dfb8dcf8ab6ed9f32cf5
alexjbustillos <alexjbustillos@gmail.com>
2020-12-16 11:27:44 -0500

    [steps]moving forward with page navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+
+import Show from './pages/Show'
+import Edit from './pages/Edit'
+import New from './pages/New'
+import Home from './pages/Home'
+
+import Nav from './components/Nav'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+
+        <h1>App!</h1>
+        <Route path='/' exact > <Home /> </Route>
+        
+        <Route path='/show/:id'>
+          <Show />
+        </Route>
+        
+        <Route path='/new' >
+          <New />
+        </Route>
+        
+        <Route path='/edit/:id' >
+          <Edit />
+        </Route>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
+        
+        
+
